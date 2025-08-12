3d25693ebd94bcf9af207145e96f8c06fd425426
Rene Brglez <rene.brglez@gmail.com>
2025-08-12 06:42:06 +0200

    WIP: tested adding scoped constraints as unmanaged xdc

--- src/base/tcl/olo_base_constraints_amd.tcl
+++ src/base/tcl/olo_base_constraints_amd.tcl
@@ -5,23 +5,23 @@ namespace eval olo_base_constraints_amd {
 
 	puts "OLO LOAD read_xdc for base"
 
-	read_xdc -quiet -ref olo_base_cc_reset $fileLoc/olo_base_cc_reset.tcl
-	read_xdc -quiet -ref olo_base_cc_bits $fileLoc/olo_base_cc_bits.tcl
-	read_xdc -quiet -ref olo_base_cc_simple $fileLoc/olo_base_cc_simple.tcl
-	read_xdc -quiet -ref olo_base_reset_gen $fileLoc/olo_base_reset_gen.tcl
-	read_xdc -quiet -ref olo_base_ram_sdp $fileLoc/olo_base_ram_sdp.tcl -unmanaged
+	read_xdc -quiet -ref olo_base_cc_reset $fileLoc/olo_base_cc_reset.tcl -unmanaged
+	read_xdc -quiet -ref olo_base_cc_bits $fileLoc/olo_base_cc_bits.tcl -unmanaged
+	read_xdc -quiet -ref olo_base_cc_simple $fileLoc/olo_base_cc_simple.tcl -unmanaged
+	read_xdc -quiet -ref olo_base_reset_gen $fileLoc/olo_base_reset_gen.tcl -unmanaged
+	#read_xdc -quiet -ref olo_base_ram_sdp $fileLoc/olo_base_ram_sdp.tcl -unmanaged
 
 	set_property used_in_synthesis false [get_files $fileLoc/olo_base_cc_reset.tcl]
 	set_property used_in_synthesis false [get_files $fileLoc/olo_base_cc_bits.tcl]
 	set_property used_in_synthesis false [get_files $fileLoc/olo_base_cc_simple.tcl]
 	set_property used_in_synthesis false [get_files $fileLoc/olo_base_reset_gen.tcl]
-	set_property used_in_synthesis false [get_files $fileLoc/olo_base_ram_sdp.tcl]
+	#set_property used_in_synthesis false [get_files $fileLoc/olo_base_ram_sdp.tcl]
 
 	set_property PROCESSING_ORDER LATE [get_files $fileLoc/olo_base_cc_reset.tcl]
 	set_property PROCESSING_ORDER LATE [get_files $fileLoc/olo_base_cc_bits.tcl]
 	set_property PROCESSING_ORDER LATE [get_files $fileLoc/olo_base_cc_simple.tcl]
 	set_property PROCESSING_ORDER LATE [get_files $fileLoc/olo_base_reset_gen.tcl]
-	set_property PROCESSING_ORDER LATE [get_files $fileLoc/olo_base_ram_sdp.tcl]
+	#set_property PROCESSING_ORDER LATE [get_files $fileLoc/olo_base_ram_sdp.tcl]
 
 	set_msg_config -id {Designutils 20-1281} -new_severity WARNING
 }
