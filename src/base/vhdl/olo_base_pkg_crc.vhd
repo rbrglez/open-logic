ba58b87db30ba50b843a6539db929189e52e9803
Rene Brglez <rene.brglez@gmail.com>
2025-12-26 09:42:25 +0100

    WIP: pkg_crc: remove unused libraries

--- src/base/vhdl/olo_base_pkg_crc.vhd
+++ src/base/vhdl/olo_base_pkg_crc.vhd
@@ -19,11 +19,6 @@
 ---------------------------------------------------------------------------------------------------
 library ieee;
     use ieee.std_logic_1164.all;
-    use ieee.numeric_std.all;
-
--- TODO: Remove?
-library work;
-    use work.olo_base_pkg_math.all;
 
 ---------------------------------------------------------------------------------------------------
 -- Package Header
